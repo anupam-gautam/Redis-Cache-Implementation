5d09e3b958eb9cd175cfda5c263c4da47aaac22f
Anupam Gautam <62124441+anupam-gautam@users.noreply.github.com>
2022-11-24 11:10:44 +0545

    Redis Cache Easy Implementation

---
+++ CacheService.cs
@@ -0,0 +1,35 @@
+﻿using LearnAndEarn.DataAccess.Context;
+using LearnAndEarn.Model.Models;
+using LearnAndEarn.Services.IServices;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+public class CacheService : ICacheService
+{
+    private IDatabase db;
+
+    public CacheService(IConnectionMultiplexer cache)
+    {
+        db = cache.GetDatabase();
+    }
+
+    public string CheckValueInCache(int key)
+    {
+        var cacheValues = db.StringGet("KeyId" + key);
+        if (cacheValues.HasValue == true)
+        {
+            return cacheValues;
+        }
+        return null;
+    }
+
+    public bool PutValueInCache(int key, object data)
+    {
+        var result = db.StringSet("quizSetId_" + key, (JsonConvert.SerializeObject(data)));
+        return result;
+    }
+
+}
---
+++ ICacheService.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnAndEarn.Services.IServices
+{
+    public interface ICacheService
+    {
+        public string CheckValueInCache(int key);
+        public bool PutValueInCache(int key, object data);
+    }
+
+}
+
